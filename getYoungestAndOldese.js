57669dbe1e545cf80802a69762cdc81a7b651c96
Sameer Chand <sam@manishas-MacBook-Air.local>
2025-02-15 12:16:16 +0530

    young and old with dept

--- getYoungestAndOldese.js
+++ getYoungestAndOldese.js
@@ -1,23 +1,24 @@
 const people = require("./people");
 
 const getYoungestAndOldest = (people) => {
-  const youngAndOld = people.reduce(
-    (accumulator, person) => {
-      if (person.age < accumulator.youngest.age) {
-        accumulator.youngest.name = person.name;
-        accumulator.youngest.age = person.age;
-      }
-      if (person.age > accumulator.oldest.age) {
-        accumulator.oldest.name = person.name;
-        accumulator.oldest.age = person.age;
-      }
-      return accumulator;
-    },
-    {
-      youngest: { name: "", age: 100000 },
-      oldest: { name: "", age: 0 },
+  const youngAndOld = people.reduce((accumulator, person) => {
+    const profession = person.profession;
+    if (!accumulator[profession]) {
+      accumulator[profession] = {
+        youngest: { name: "", age: 100000 },
+        oldest: { name: "", age: 0 },
+      };
     }
-  );
+    if (person.age < accumulator[profession].youngest.age) {
+      accumulator[profession].youngest.name = person.name;
+      accumulator[profession].youngest.age = person.age;
+    }
+    if (person.age > accumulator[profession].oldest.age) {
+      accumulator[profession].oldest.name = person.name;
+      accumulator[profession].oldest.age = person.age;
+    }
+    return accumulator;
+  }, {});
 
   return youngAndOld;
 };
